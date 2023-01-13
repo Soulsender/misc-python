61abc3086cd04b00699d760c2044be5101e47723
Soulsender <soulsender@tutanota.com>
2023-01-13 17:24:47 +0000

    Update memebot.py

--- memebot.py
+++ memebot.py
@@ -13,7 +13,7 @@ def main():
     intents.members = True
     intents.message_content = True
     activity = nextcord.Activity(
-        type=nextcord.ActivityType.listening, name=f"/help"
+        type=nextcord.ActivityType.listening, name=f"calls to frongation"
     )
 
     bot = commands.Bot(
@@ -25,6 +25,7 @@ def main():
 
     # boolean that will be set to true when views are added
     bot.persistent_views_added = False
+
     @bot.event
     async def on_ready():
         print(f"USER: {bot.user} URL:", f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&scope=applications.commands%20bot")
@@ -36,20 +37,21 @@ def main():
     async def on_message(message):
         if message.author == bot.user:
             return
-        frong_words = ["frong", "Frong"]
+        frong_words = ["frong"]
         arch_words = ["arch", "Arch", "archlinux"]
         for word in arch_words:
             if word in message.content:
-                await message.channel.send('🤓')
-                await message.channel.send('i uSe ArCh bTW!!!1!11')
+                await message.channel.send('yOu SAiD aRcH!!11!1! i uSe ArCh bTW!!!1!11 🤓🤓🤓')
+                break
         for word in frong_words:
             if word in message.content:
-                await message.channel.send('frong :frong:')
+                await message.channel.send('frong', files=[nextcord.File('/home/user/Shitbot/frong.jpeg')])
+                break
 
 
-#     for filename in os.listdir('./cogs'):
-#         if filename.endswith('.py'):
-#             bot.load_extension(f'cogs.{filename[:-3]}')
+    for filename in os.listdir('/home/user/Shitbot/cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
 
     async def startup():
         bot.session = aiohttp.ClientSession()
